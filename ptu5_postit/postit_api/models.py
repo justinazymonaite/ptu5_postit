0eb62f11dfd46a192ab257321052a6a54dd8adf9
Justina Zymonaite <jzymonaite@yahoo.com>
2022-11-24 13:43:50 +0200

    postu kurimas

---
+++ ptu5_postit/postit_api/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class Post(models.Model):
+    title = models.CharField(_("title"), max_length=50)
+    body = models.TextField(_("body"))
+    user = models.ForeignKey(
+        User, verbose_name=_("user"), 
+        on_delete=models.CASCADE, 
+        related_name="posts",
+    )
+    created_at = models.DateTimeField(_("created at"), auto_now_add=True)
+
+    def __str__(self) -> str:
+        return _("{title} by {user} posted at {created_at}").format(
+            title=self.title,
+            user=self.user,
+            created_at=self.created_at,
+        )
+
+    class Meta:
+        ordering = ('-created_at', )
---
+++ ptu5_postit/postit_api/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from rest_framework import generics, permissions
+from . import models, serializers
+
+
+
+
+class PostList(generics.ListCreateAPIView):
+    queryset = models.Post.objects.all()
+    serializer_class = serializers.PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
